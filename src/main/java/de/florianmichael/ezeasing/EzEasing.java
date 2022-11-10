6a9f8f45a72410325a6ee1fcdb9d447b5cce4717
Sumandora <52950755+Sumandora@users.noreply.github.com>
2022-11-10 01:11:54 +0100

    Reimplement all functions

--- src/main/java/de/florianmichael/ezeasing/EzEasing.java
+++ src/main/java/de/florianmichael/ezeasing/EzEasing.java
@@ -11,86 +11,85 @@ public enum EzEasing {
 
     LINEAR("Linear", x -> x),
 
-    IN_SINE("In sine", x -> (float) (1 - Math.cos(x * Math.PI) / 2F)),
-    OUT_SINE("Out sine", x -> (float) (1 - Math.sin(x * Math.PI) / 2F)),
-    IN_OUT_SINE("In out sine", x -> (float) (-(Math.cos(Math.PI * x) - 1) / 2F)),
+    IN_SINE("In sine", x -> (float) (1F - Math.cos((x * Math.PI) / 2F))),
+    OUT_SINE("Out sine", x -> (float) (1F - Math.sin((x * Math.PI) / 2F))),
+    IN_OUT_SINE("In out sine", x -> (float) (-(Math.cos(Math.PI * x) - 1F) / 2F)),
 
     IN_QUAD("In quad", x -> x * x),
-    OUT_QUAD("Out quad", x -> 1 - (1 - x) * (1 - x)),
-    IN_OUT_QUAD("In out quad", x -> x < 0.5 ? 2 * x * x : 1 - (float) Math.pow(-2 * x + 2, 2) / 2F),
+    OUT_QUAD("Out quad", x -> 1F - (1F - x) * (1F - x)),
+    IN_OUT_QUAD("In out quad", x -> x < 0.5F ? 2F * x * x : 1F - (float) Math.pow(-2F * x + 2F, 2F) / 2F),
 
-    IN_CUBIC("In cubic", x -> (float) Math.pow(x, 3)),
-    OUT_CUBIC("Out cubic", x -> (float) (1 - Math.pow(1 - x, 3))),
-    IN_OUT_CUBIC("In out cubic", x -> x < 0.5 ? 4 * x * x * x : 1 - (float) Math.pow(-2 * x + 2, 3) / 2F),
+    IN_CUBIC("In cubic", x -> (float) Math.pow(x, 3F)),
+    OUT_CUBIC("Out cubic", x -> (float) (1F - Math.pow(1F - x, 3F))),
+    IN_OUT_CUBIC("In out cubic", x -> x < 0.5F ? 4F * x * x * x : 1F - (float) Math.pow(-2F * x + 2F, 3F) / 2F),
 
-    IN_QUART("In quart", x -> (float) Math.pow(x, 4)),
-    OUT_QUART("Out quart", x -> (float) (1 - Math.pow(1 - x, 4))),
-    IN_OUT_QUART("In out quart", x -> x < 0.5 ? 8 * x * x * x * x : 1 - (float) Math.pow(-2 * x + 2, 4) / 2),
+    IN_QUART("In quart", x -> (float) Math.pow(x, 4F)),
+    OUT_QUART("Out quart", x -> (float) (1F - Math.pow(1F - x, 4F))),
+    IN_OUT_QUART("In out quart", x -> x < 0.5F ? 8F * x * x * x * x : 1F - (float) Math.pow(-2F * x + 2F, 4F) / 2F),
 
-    IN_QUINT("In quint", x -> (float) Math.pow(x, 5)),
-    OUT_QUINT("Out quint", x -> (float) (1 - Math.pow(1 - x, 5))),
-    IN_OUT_QUINT("In out quint", x -> x < 0.5 ? 16 * x * x * x * x * x : 1 - (float) Math.pow(-2 * x + 2, 5) / 2F),
+    IN_QUINT("In quint", x -> (float) Math.pow(x, 5F)),
+    OUT_QUINT("Out quint", x -> (float) (1F - Math.pow(1F - x, 5F))),
+    IN_OUT_QUINT("In out quint", x -> x < 0.5F ? 16F * x * x * x * x * x : 1F - (float) Math.pow(-2F * x + 2F, 5F) / 2F),
 
-    IN_EXPO("In expo", x -> x == 0 ? 0 : (float) Math.pow(2, 10 * x - 10)),
-    OUT_EXPO("Out expo", x -> x == 1 ? 1 : (float) (1 - Math.pow(2, -10 * x))),
-    IN_OUT_EXPO("In out expo", x -> x == 0 ? 0 : (float) (x == 1 ? 1 : x < 0.5 ? Math.pow(2, 20 * x - 10) / 2 : (2 - Math.pow(2, -20 * x + 10)) / 2)),
+    IN_EXPO("In expo", x -> x == 0F ? 0F : (float) Math.pow(2F, 10F * x - 10F)),
+    OUT_EXPO("Out expo", x -> x == 1F ? 1F : 1F - (float) Math.pow(2F, -10F * x)),
+    IN_OUT_EXPO("In out expo", x -> x == 0F ? 0F : x == 1F ? 1F : x < 0.5F ? (float) Math.pow(2F, 20F * x - 10F) / 2F : (2F - (float) Math.pow(2F, -20F * x + 10F)) / 2F),
 
-    IN_CIRC("In circ", x -> (float) (1 - Math.sqrt(1 - Math.pow(x, 2)))),
-    OUT_CIRC("Out circ", x -> (float) Math.sqrt(1 - Math.pow(x - 1, 2))),
-    IN_OUT_CIRC("In out circ", x -> (float) (x < 0.5 ? (1 - Math.sqrt(1 - Math.pow(2 * x, 2))) / 2 : (Math.sqrt(1 - Math.pow(-2 * x + 2, 2)) + 1) / 2)),
+    IN_CIRC("In circ", x -> 1F - (float) Math.sqrt(1F - (float) Math.pow(x, 2F))),
+    OUT_CIRC("Out circ", x -> (float) Math.sqrt(1F - (float) Math.pow(x - 1F, 2F))),
+    IN_OUT_CIRC("In out circ", x -> x < 0.5F ? (1F - (float) Math.sqrt(1 - (float) Math.pow(2F * x, 2F))) / 2F : ((float) Math.sqrt(1F - (float) Math.pow(-2F * x + 2F, 2F)) + 1F) / 2F),
 
     IN_BACK("In back", x -> {
         final float c1 = 1.70158F;
-        final float c3 = c1 + 1;
+        final float c3 = c1 + 1F;
 
         return c3 * x * x * x - c1 * x * x;
     }),
     OUT_BACK("Out back", x -> {
         final float c1 = 1.70158F;
-        final float c3 = c1 + 1;
+        final float c3 = c1 + 1F;
 
-        return 1 + c3 * (float) Math.pow(x - 1, 3) + c1 * (float) Math.pow(x - 1, 2);
+        return 1F + c3 * (float) Math.pow(x - 1F, 3F) + c1 * (float) Math.pow(x - 1F, 2F);
     }),
     IN_OUT_BACK("In out back", x -> {
         final float c1 = 1.70158F;
         final float c2 = c1 * 1.525F;
 
-        return x < 0.5 ? ((float) Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2 : ((float) Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+        return x < 0.5F ? ((float) Math.pow(2F * x, 2F) * ((c2 + 1F) * 2F * x - c2)) / 2F : ((float) Math.pow(2F * x - 2F, 2F) * ((c2 + 1F) * (x * 2F - 2F) + c2) + 2F) / 2F;
     }),
 
     IN_ELASTIC("In elastic", x -> {
         final float c4 = (2F * (float) Math.PI) / 3F;
 
-        return x == 0 ? 0 : (float) (x == 1 ? 1 : -Math.pow(2, 10 * x - 10) * Math.sin((x * 10 - 10.75) * c4));
+        return x == 0F ? 0F : x == 1F ? 1F : (float) -Math.pow(2F, 10F * x - 10F) * (float) Math.sin((x * 10F - 10.75F) * c4);
     }),
     OUT_ELASTIC("Out elastic", x -> {
         final float c4 = (2F * (float) Math.PI) / 3F;
 
-        return x == 0 ? 0 : (float) (x == 1 ? 1 : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1);
+        return x == 0F ? 0F : x == 1F ? 1F : (float) Math.pow(2F, -10F * x) * (float) Math.sin((x * 10F - 0.75F) * c4) + 1F;
     }),
     INT_OUT_ELASTIC("In out elastic", x -> {
-        final double c5 = (2 * Math.PI) / 4.5;
-        final double sin = Math.sin((20 * x - 11.125) * c5);
+        final float c5 = (2F * (float) Math.PI) / 4.5F;
 
-        return x == 0 ? 0 : (float) (x == 1 ? 1 : x < 0.5 ? -(Math.pow(2, 20 * x - 10) * sin) / 2 : (Math.pow(2, -20 * x + 10) * sin) / 2 + 1);
+        return x == 0F ? 0F : x == 1F ? 1F : x < 0.5F ? -((float) Math.pow(2F, 20F * x - 10F) * (float) Math.sin((20F * x - 11.125F) * c5)) / 2F : ((float) Math.pow(2F, -20F * x + 10F) * (float) Math.sin((20F * x - 11.125F) * c5)) / 2F + 1F;
     }),
 
     OUT_BOUNCE("Out bounce", x -> {
         final float n1 = 7.5625F;
         final float d1 = 2.75F;
 
-        if (x < 1 / d1) {
+        if (x < 1F / d1) {
             return n1 * x * x;
-        } else if (x < 2 / d1) {
+        } else if (x < 2F / d1) {
             return n1 * (x -= 1.5F / d1) * x + 0.75F;
-        } else if (x < 2.5 / d1) {
+        } else if (x < 2.5F / d1) {
             return n1 * (x -= 2.25F / d1) * x + 0.9375F;
         } else {
             return n1 * (x -= 2.625F / d1) * x + 0.984375F;
         }
     }),
-    IN_BOUNCE("In bounce", x -> 1 - EzEasing.OUT_BOUNCE.ease(1 - x)),
-    IN_OUT_BOUNCE("In out bounce", x -> x < 0.5 ? (1 - EzEasing.OUT_BOUNCE.ease(1 - 2 * x)) / 2 : (1 + EzEasing.OUT_BOUNCE.ease(2 * x - 1)) / 2);
+    IN_BOUNCE("In bounce", x -> 1F - EzEasing.OUT_BOUNCE.ease(1F - x)),
+    IN_OUT_BOUNCE("In out bounce", x -> x < 0.5F ? (1F - EzEasing.OUT_BOUNCE.ease(1F - 2F * x)) / 2F : (1F + EzEasing.OUT_BOUNCE.ease(2F * x - 1F)) / 2F);
 
     public final String name;
     public final Function<Float, Float> function;
